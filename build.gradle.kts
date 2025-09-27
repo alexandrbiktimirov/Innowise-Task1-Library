3194a448b40cb96924a987f7310825f53dad0b8f
Alexandr Biktimirov <alex.biktimirov38@gmail.com>
2025-09-27 12:34:01 +0200

    feat: add logging and aop dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,10 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework:spring-aop:5.3.39")
+    implementation("org.aspectj:aspectjweaver:1.9.22.1")
+    implementation("org.slf4j:slf4j-api:2.0.16")
+    runtimeOnly("ch.qos.logback:logback-classic:1.5.13")
     implementation("org.springframework:spring-context:7.0.0-M1")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.20.0")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
