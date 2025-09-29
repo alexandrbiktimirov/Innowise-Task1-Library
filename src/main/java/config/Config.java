e898315f3233d7e7034338d4ba8c3d895612ec26
Alexandr Biktimirov <alex.biktimirov38@gmail.com>
2025-09-29 12:50:24 +0200

    feat: add logging aspect

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -1,11 +1,19 @@
 package config;
 
+import aop.LoggingAspect;
 import controller.BookController;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import service.BookService;
 
 @org.springframework.context.annotation.Configuration
+@EnableAspectJAutoProxy
 public class Config {
+
+    @Bean
+    public LoggingAspect loggingAspect() {
+        return new LoggingAspect();
+    }
 
     @Bean
     public BookService bookService(){
