3996d88c0a2ad82a682efa9465cde040dfb678f1
Alexandr Biktimirov <alex.biktimirov38@gmail.com>
2025-12-09 15:23:07 +0100

    chore: update TOML file to organize dependencies better

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,40 +1,34 @@
 plugins {
-    id("org.springframework.boot") version "3.4.0"
-    id("io.spring.dependency-management") version "1.1.6"
-    id("java")
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.java)
 }
-
-group = "org.example"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
-val junitBom = "5.10.2"
-
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-aop")
-    implementation("org.mapstruct:mapstruct:1.6.3")
-    implementation("org.postgresql:postgresql:42.7.7")
-    implementation("org.aspectj:aspectjweaver:1.9.22.1")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("jakarta.annotation:jakarta.annotation-api:3.0.0")
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("io.jsonwebtoken:jjwt-api:0.13.0")
-    testImplementation(platform("org.junit:junit-bom:$junitBom"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.springframework.security:spring-security-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(libs.bundles.spring.boot.starter.implementation)
 
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.13.0")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.13.0")
+    implementation(libs.mapstruct)
+    implementation(libs.postgresql)
+    implementation(libs.aspectjweaver)
+    implementation(libs.jakarta.annotation)
+    implementation(libs.jjwt.api)
 
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+    runtimeOnly(libs.jjwt.impl)
+    runtimeOnly(libs.jjwt.jackson)
+
+    testImplementation(platform(libs.junit.bom))
+    testImplementation(libs.bundles.testcontainers.all)
+    testImplementation(libs.junit.jupiter)
+    testImplementation(libs.spring.security.test)
+    testImplementation(libs.spring.boot.test)
+    testRuntimeOnly(libs.junit.platform.launcher)
+
+    annotationProcessor(libs.spring.boot.configuration.processor)
+    annotationProcessor(libs.mapstruct.processor)
 }
 
 tasks.test {
