7b267efd8700456887750cd4b4ee974b0e295743
Alexandr Biktimirov <alex.biktimirov38@gmail.com>
2026-02-23 16:26:53 +0100

    fix: add entity graph for finding all books

--- src/main/java/com/example/library/repository/BookRepository.java
+++ src/main/java/com/example/library/repository/BookRepository.java
@@ -1,7 +1,12 @@
 package com.example.library.repository;
 
 import com.example.library.model.Book;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+    @EntityGraph(attributePaths = {"authors", "genres"})
+    List<Book> findAll();
 }
